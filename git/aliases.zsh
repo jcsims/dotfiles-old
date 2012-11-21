516601ea9d53b6ca46836ad16d7bcee2f0b56168
Chris Sims <chris@jcsims.me>
2012-11-21 17:25:12 -0500

    Git changes

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias gpl='git pull'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
@@ -17,3 +18,5 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias ga='git add'
+
