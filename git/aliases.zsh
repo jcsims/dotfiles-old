9253c8561ac8be1d43bbb29d18e87e3ff470bbda
Chris Sims <chris@jcsims.me>
2012-12-03 17:08:05 -0500

    Reset all the things

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,4 +19,5 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias ga='git add'
+alias grs='git reset'
 
