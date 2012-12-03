58cbc1c7d6de7a3db4f4aa99a6d197cfed694284
Chris Sims <chris@jcsims.me>
2012-12-03 16:44:50 -0500

    Add a short fact during every pull

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,10 +7,10 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
+alias gl='fact && git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
-alias gpl='git pull'
+alias gpl='fact && git pull --rebase'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,2 @@
 alias reload!='. ~/.zshrc'
+alias fact="elinks -dump randomfunfacts.com | sed -n '/^| /p' | tr -d \|"
