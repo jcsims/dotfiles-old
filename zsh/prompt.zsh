bc25fb458a86526e2f312080459ee4f0448493e0
Chris Sims <chris@jcsims.me>
2012-12-08 19:57:06 -0500

    Added machine name to prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -72,7 +72,11 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+machine_name(){
+  echo "%{$fg_bold[green]%}$(echo $HOST | sed 's/\..*$//')%{$reset_color%}"
+}
+
+export PROMPT=$'\n$(rb_prompt) in $(machine_name):$(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
