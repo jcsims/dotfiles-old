21d8dbf68d8f6bfe9a8d67a750722bbc8bce2603
Chris Sims <chris@jcsims.me>
2013-07-21 19:08:04 -0400

    Add Emacs back <_<

---
+++ emacs/emacs.d.symlink/init.el
@@ -0,0 +1,67 @@
+;; Package Management
+;; Make sure that all the packages I use are installed
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-eshell
+                      clojure-mode
+                      clojure-test-mode
+                      nrepl
+                      auctex
+                      magit
+                      monokai-theme
+                      expand-region
+                      auto-complete
+                      ac-nrepl))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; Load other files
+(load "~/.emacs.d/funcs.el")
+
+;; Aesthetics
+(load-theme 'monokai t)
+
+;; Enable whitespace mode for progamming languages, and highlight when
+;; lines are over 80 characters long
+(setq whitespace-line-column 80)
+(setq whitespace-style '(face lines-tail))
+(add-hook 'prog-mode-hook 'whitespace-mode)
+
+;; Clojure mode
+(add-hook 'clojure-mode-hook 'paredit-mode)
+
+;; Save backups to a central location
+;; Taken from http://whattheemacsd.com/init.el-02.html
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
+;; Clean up some noisy modes
+(rename-modeline "clojure-mode" clojure-mode "Clj")
+
+;; Globally enable yasnippets
+(yas-global-mode 1)
+
+;; Configure auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+(global-auto-complete-mode 1)
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'nrepl-mode))
+
+;; Bind expand-region
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
