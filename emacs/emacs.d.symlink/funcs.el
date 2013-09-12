cf459b044a20a6659c6ad8cab2fdfc482299a458
Chris Sims <chris@jcsi.ms>
2013-09-12 15:33:34 -0400

    Added remaining stuff pulled from the starter kit.

--- emacs/emacs.d.symlink/funcs.el
+++ emacs/emacs.d.symlink/funcs.el
@@ -53,16 +53,6 @@ point reaches the beginning or end of buffer, stop there."
      '(defadvice ,mode (after rename-modeline activate)
         (setq mode-name ,new-name))))
 
-;; Highlight annotations in comments, take from Emacs Redux
-;; http://emacsredux.com/blog/2013/07/24/highlight-comment-annotations/
-(defun font-lock-comment-annotations ()
-  "Highlight a bunch of well-known comment annotations."
-  (font-lock-add-keywords
-   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\):"
-          1 font-lock-warning-face t))))
-
-(add-hook 'prog-mode-hook 'font-lock-comment-annotations)
-
 ;; Taken from technomancy's emacs.d
 (global-set-key (kbd "C-c n")
                 (defun pnh-cleanup-buffer () (interactive)
@@ -71,3 +61,38 @@ point reaches the beginning or end of buffer, stop there."
                   (indent-region (point-min) (point-max))))
 
 ;; Some functions carried over from the emacs starter kit
+(defun esk-local-column-number-mode ()
+  (make-local-variable 'column-number-mode)
+  (column-number-mode t))
+
+(defun esk-local-comment-auto-fill ()
+  (set (make-local-variable 'comment-auto-fill-only-comments) t)
+  (auto-fill-mode t))
+
+(defun esk-turn-on-hl-line-mode ()
+  (when (> (display-color-cells) 8)
+    (hl-line-mode t)))
+
+(defun esk-turn-on-save-place-mode ()
+  (require 'saveplace)
+  (setq save-place t))
+
+(defun esk-pretty-lambdas ()
+  (font-lock-add-keywords
+   nil `(("(?\\(lambda\\>\\)"
+          (0 (progn (compose-region (match-beginning 1) (match-end 1)
+                                    ,(make-char 'greek-iso8859-7 107))
+                    nil))))))
+
+(defun esk-add-watchwords ()
+  (font-lock-add-keywords
+   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|HACK\\|REFACTOR\\|NOCOMMIT\\)"
+          1 font-lock-warning-face t))))
+
+(add-hook 'prog-mode-hook 'esk-local-column-number-mode)
+(add-hook 'prog-mode-hook 'esk-local-comment-auto-fill)
+(add-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
+(add-hook 'prog-mode-hook 'esk-turn-on-save-place-mode)
+(add-hook 'prog-mode-hook 'esk-pretty-lambdas)
+(add-hook 'prog-mode-hook 'esk-add-watchwords)
+(add-hook 'prog-mode-hook 'idle-highlight-mode)
