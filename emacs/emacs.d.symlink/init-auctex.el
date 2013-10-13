20b018db8846372936a7ee67dade198922a2beae
Chris Sims <chris@jcsi.ms>
2013-10-13 16:15:16 -0400

    Switch back to compiled auctex.
    This works around a strange issue cropping up in the latest
    versions. Eventually I should be able to use auctex from the
    repo again.

--- emacs/emacs.d.symlink/init-auctex.el
+++ emacs/emacs.d.symlink/init-auctex.el
@@ -1,11 +1,19 @@
 ;;; AUCTex settings
-(use-package auctex
-  :ensure t
-  :init
-  (progn
-    (setq TeX-auto-save t)
-    (setq TeX-parse-self t)
-    (setq-default TeX-master nil)))
+; Since the latest is installed from source,
+; need to make sure it's loaded
+(setq TeX-data-directory "~/code/auctex/")
+(add-to-list 'load-path "~/code/auctex/")
+(add-to-list 'load-path "~/code/auctex/preview/")
+(add-to-list 'Info-directory-list "~/code/auctex/doc")
+
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+;; I always use pdf
+(setq TeX-PDF-mode t)
 
 (provide 'init-auctex)
 ;;; init-auctex.el ends here
