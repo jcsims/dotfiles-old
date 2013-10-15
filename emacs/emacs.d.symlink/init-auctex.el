8f4d4d0e4a1c94bb0685f9f0f21beff21dcf6c42
Chris Sims <chris@jcsi.ms>
2013-10-14 20:41:01 -0400

    Be smarter about where auctex comes from

--- emacs/emacs.d.symlink/init-auctex.el
+++ emacs/emacs.d.symlink/init-auctex.el
@@ -1,13 +1,17 @@
 ;;; AUCTex settings
-; Since the latest is installed from source,
-; need to make sure it's loaded
-(setq TeX-data-directory "~/code/auctex/")
-(add-to-list 'load-path "~/code/auctex/")
-(add-to-list 'load-path "~/code/auctex/preview/")
-(add-to-list 'Info-directory-list "~/code/auctex/doc")
+;; If the latest is installed from source,
+;; use that
+(if (file-directory-p "~/code/auctex")
+    (progn
+      (setq TeX-data-directory "~/code/auctex/")
+      (add-to-list 'load-path "~/code/auctex/")
+      (add-to-list 'load-path "~/code/auctex/preview/")
+      (add-to-list 'Info-directory-list "~/code/auctex/doc"))
+  (install-package auctex))
 
 (load "auctex.el" nil t t)
 (load "preview-latex.el" nil t t)
+
 
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
