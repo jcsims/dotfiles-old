7b9ceb2175955c47e302d3591588830fc2b7abda
Chris Sims <chris@jcsi.ms>
2013-11-16 08:57:47 -0500

    Automatically start an Emacs server if it's not running

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,7 @@ alias reload!='. ~/.zshrc'
 alias fact="elinks -dump randomfunfacts.com | sed -n '/^| /p' | tr -d \|"
 alias tree='tree -C'
       
-alias e='emacsclient -t'
-alias ec='emacsclient -c'
-alias vi='emacsclient -t'
-alias vim='emacsclient -t'
+alias e='emacsclient -t -a ""'
+alias ec='emacsclient -c -a ""'
+alias vi='emacsclient -t -a ""'
+alias vim='emacsclient -t -a ""'
