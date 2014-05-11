fd0f9dde8baeb5d4e6496b84f6cd982a45922125
Chris Sims <chris@jcsi.ms>
2014-05-11 15:19:26 -0700

    Spiffy R highlighting.

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -126,4 +126,18 @@
 (global-set-key (kbd "C-c e t")
                 (lambda () (interactive) (find-file "~/org/todo.org")))
 
+;; Emacs Speaks Statistics
+(require 'ess-site)
+(setq ess-R-font-lock-keywords '((ess-R-fl-keyword:modifiers . t)
+                                 (ess-R-fl-keyword:fun-defs . t)
+                                 (ess-R-fl-keyword:keywords . t)
+                                 (ess-R-fl-keyword:assign-ops . t)
+                                 (ess-R-fl-keyword:constants . t)
+                                 (ess-fl-keyword:fun-calls . t)
+                                 (ess-fl-keyword:numbers . t)
+                                 (ess-fl-keyword:operators . t)
+                                 (ess-fl-keyword:delimiters . t)
+                                 (ess-fl-keyword:= . t)
+                                 (ess-R-fl-keyword:F&T . t)))
+
 ;;; init.el ends here
