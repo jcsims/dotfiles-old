66e1e40f0c8e7d83dbf0384fe0d8b9b65beda28b
Chris Sims <chris@jcsi.ms>
2014-05-21 08:45:01 -0700

    Pull in a few changes from upstream.

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,17 +1,5 @@
 # GRC colorizes nifty unix tools all over the place
-GRC=`which grc`
-if [ "$TERM" != dumb ] && [ -n "$GRC" ]
+if (( $+commands[grc] )) && (( $+commands[brew] ))
 then
-    alias colourify="$GRC -es --colour=auto"
-    alias configure='colourify ./configure'
-    alias diff='colourify diff'
-    alias make='colourify make'
-    alias gcc='colourify gcc'
-    alias g++='colourify g++'
-    alias as='colourify as'
-    alias gas='colourify gas'
-    alias ld='colourify ld'
-    alias netstat='colourify netstat'
-    alias ping='colourify ping'
-    alias traceroute='colourify /usr/sbin/traceroute'
+  source `brew --prefix`/etc/grc.bashrc
 fi
