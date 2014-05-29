5fbce9fb2ba21fcfe256b3382d2d68957fbc79b0
Chris Sims <chris@jcsi.ms>
2014-05-29 11:45:59 -0700

    Preliminary haskell-mode changes.

--- emacs.d/lisp/init-haskell.el
+++ emacs.d/lisp/init-haskell.el
@@ -9,7 +9,33 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 
+;; Add some easy align regexp's
+;; (add-to-list 'align-rules-list
+;;              '(haskell-types
+;;                (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
+;;                (modes quote (haskell-mode literate-haskell-mode))))
+;; (add-to-list 'align-rules-list
+;;              '(haskell-assignment
+;;                (regexp . "\\(\\s-+\\)=\\s-+")
+;;                (modes quote (haskell-mode literate-haskell-mode))))
+;; (add-to-list 'align-rules-list
+;;              '(haskell-arrows
+;;                (regexp . "\\(\\s-+\\)\\(->\\|→\\)\\s-+")
+;;                (modes quote (haskell-mode literate-haskell-mode))))
+;; (add-to-list 'align-rules-list
+;;              '(haskell-left-arrows
+;;                (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+")
+;;                (modes quote (haskell-mode literate-haskell-mode))))
+
+
+;;(speedbar-add-supported-extension ".hs")
+
 (define-key haskell-mode-map (kbd "C-c n") 'haskell-mode-stylish-buffer)
+
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
 
 (provide 'init-haskell)
 ;;; init-haskell.el ends here
