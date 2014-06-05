dd1267bdef6668c1e53a61b1e949312122f7e87a
Chris Sims <chris@jcsi.ms>
2014-06-04 18:47:36 -0700

    Clean up Haskell config

--- emacs.d/lisp/init-haskell.el
+++ emacs.d/lisp/init-haskell.el
@@ -4,7 +4,7 @@
 ;; Various hooks for super Haskell power
 
 ;;; Code:
-(add-hook 'haskell-mode-hook 'turn-on-haskell-unicode-input-method)
+                                        ;(add-hook 'haskell-mode-hook 'turn-on-haskell-unicode-input-method)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
@@ -33,9 +33,33 @@
 (define-key haskell-mode-map (kbd "C-c n") 'haskell-mode-stylish-buffer)
 
 (custom-set-variables
-  '(haskell-process-suggest-remove-import-lines t)
-  '(haskell-process-auto-import-loaded-modules t)
-  '(haskell-process-log t))
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t))
+
+(eval-after-load "haskell-mode"
+  '(progn
+     (define-key haskell-mode-map (kbd "C-<") 'haskell-move-nested-left)
+     (define-key haskell-mode-map (kbd "C->") 'haskell-move-nested-right)
+     (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile)
+
+     ;; new haskell-interactive-mode
+     (define-key haskell-mode-map (kbd "C-x C-d") nil)
+     (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+     (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+     (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+     (define-key haskell-mode-map (kbd "C-c M-.") nil)
+     (define-key haskell-mode-map (kbd "C-c C-d") nil)))
+
+(eval-after-load "haskell-cabal"
+  '(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))
+
+;; Set up ghc-mod
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
 (provide 'init-haskell)
 ;;; init-haskell.el ends here
