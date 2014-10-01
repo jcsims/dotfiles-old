c0ab419b643925eac487f6248dc3b48874d74a88
Chris Sims <chris@jcsi.ms>
2014-10-01 07:57:10 -0700

    Move `custom` block to head of file.
    Resolves emacs constantly asking about `smart-mode-line` themes.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,6 +6,17 @@
 ;; troubleshoot yourself!
 
 ;;; Code:
+
+;; Add custom to the start of the file in an attempt to avoid emacs
+;; asking about smart-mode-line's theme every time on startup
+(custom-set-variables
+ '(custom-safe-themes
+   (quote
+    ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723"
+     "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e"
+     "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f"
+     default))))
+
 (add-to-list 'load-path (concat user-emacs-directory "lisp"))
 
 ;; Package Management
@@ -153,17 +164,3 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
