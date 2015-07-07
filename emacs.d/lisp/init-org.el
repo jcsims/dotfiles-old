9b80e986408f061bbebf78fb17518647894f8099
Chris Sims <chris@jcsi.ms>
2015-07-06 18:08:01 -0700

    Don't ignore org archive files, add to agenda
    Also, mark a parent tree complete if all of it's children have been
    marked complete.

--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -11,9 +11,19 @@
 (setq org-startup-indented t)
 (setq org-startup-folded t)
 
-(setq org-agenda-files (list (concat org-directory "/todo.org")))
+(setq org-agenda-files (list (concat org-directory "/todo.org")
+                             (concat org-directory "/todo.org_archive")))
 (setq org-default-notes-file
       (concat org-directory "/todo.org"))
+
+;; Taken from the org-mode manual - Automatically mark a parent task
+;; as DONE when all child nodes are marked DONE
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
 
 (provide 'init-org)
 ;;; init-org.el ends here
