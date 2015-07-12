054c37f139831fdac7e97f536d0d120272defe95
Chris Sims <chris@jcsi.ms>
2015-07-12 14:58:53 -0700

    Turn on yasnippet in clojure buffers

--- emacs.d/lisp/init-clojure.el
+++ emacs.d/lisp/init-clojure.el
@@ -16,6 +16,7 @@
 (require 'clj-refactor)
 (add-hook 'clojure-mode-hook (lambda ()
                                (clj-refactor-mode 1)
+                               (yas-minor-mode 1)
                                (cljr-add-keybindings-with-prefix "C-c r")))
 
 
