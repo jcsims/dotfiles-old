6be082c147715a0c1a62d3b85aa71b00858a6291
Chris Sims <chris@jcsi.ms>
2019-08-07 12:59:19 -0700

    Add workaround for bug in Emacs with TLS1.3

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -33,6 +33,13 @@
       use-package-verbose t)
 (use-package bind-key)
 
+; Some combination of GNU TLS and Emacs fail to retrieve archive
+; contents over https.
+; https://www.reddit.com/r/emacs/comments/cdei4p/failed_to_download_gnu_archive_bad_request/etw48ux
+; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
+(if (and (version< emacs-version "26.3") (>= libgnutls-version 30600))
+    (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+
 (use-package no-littering
   :config
   (require 'files)
